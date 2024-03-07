74d05f171de35f2635c7b65823571fd9c81e5e81
jolobos <digo8432@gmail.com>
2024-03-06 22:27:21 -0300

    1 commit estruturando a pagina nfo para ser meu index - carrossel

---
+++ info.php
@@ -0,0 +1,107 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>HOME - DL Store</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  
+  
+  </head>
+  <body>
+    
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<div class="container">  <a class="navbar-brand" href="#">Home</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Alterna navegação">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="container collapse navbar-collapse" id="navbarTogglerDemo02">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Produtos</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Serviços</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Endereço</a>
+      </li>
+    </ul>
+   </div>
+   <form class="form-inline my-2 my-lg-0 w-75 p-3 ">
+   <div class="row ">
+   <div class="col "  >
+      <input class="form-control mr-sm-2" type="search"  placeholder="Pesquisar">
+	  </div>
+	     <div class="col" >
+      <button class="btn btn-outline-success" type="submit">Pesquisar</button>
+	  </div>
+	  </div>
+    </form>
+	</div>
+</nav>
+
+<div id="demo" class="carousel slide" data-bs-ride="carousel">
+	<div class="carousel-indicators">
+		<button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
+		<button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
+		<button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
+	</div>
+
+	<div class="carousel-inner">
+		<div class="carousel-item active">
+		  <img src="img/img1.jpg" alt="PC GAMER" class="d-block w-100" style="height: 500px">
+		  <div class="carousel-caption">
+			<h3 class="text-secondary">Monte seu PC Gamer</h3>
+			<a class="btn btn-danger text-dark" href="#">Click no botão e saiba mais</a>
+		  </div>
+		</div>
+		<div class="carousel-item">
+		  <img src="img/img7.jpg" alt="Celulares" class="d-block w-100" style="height: 500px">
+		  <div class="carousel-caption">
+			<h3 class="text-secondary">Consulte nossos preços</h3>
+			<a class="btn btn-danger text-dark" href="#">Click no botão e saiba mais</a>
+		  </div>
+		</div>
+		<div class="carousel-item">
+		  <img src="img/img3.jpg" alt="New York" class="d-block w-100" style="height: 500px">
+		  <div class="carousel-caption">
+			<h3 class="text-secondary">Amplo estoque em Carregadores</h3>
+			<a class="btn btn-danger text-dark" href="#">Click no botão e saiba mais</a>
+		  </div>
+		</div>
+	</div>
+
+	<button class="carousel-control-prev " type="button" data-bs-target="#demo" data-bs-slide="prev">
+		<span class="carousel-control-prev-icon bg-dark" style="border-radius: 30%"></span>
+	</button>
+	<button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+	<span class="carousel-control-next-icon bg-dark" style="border-radius: 30%"></span>
+	  </button>
+</div>
+
+
+	<h1>Hello, world!</h1>
+</br>	
+</br>	
+</br>
+<div class="container mt-3">	
+	<div class="dropdown">
+		<button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown">
+		Botao de escolha
+		</button>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="login.php">Link 1</a></li>
+    <li><a class="dropdown-item" href="#">Link 2</a></li>
+    <li><a class="dropdown-item" href="#">Link 3</a></li>
+  </ul>
+</div>
+</div>
+	
+	
+	
+	
+	</body>
+</html>
