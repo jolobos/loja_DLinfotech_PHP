a65f54511b379675d62d213a30f3c84fe142a041
jolobos <digo8432@gmail.com>
2024-03-09 17:57:02 -0300

    separei o cabeçalho em uma pagina que pode ser chamada pelo require once

---
+++ cabecalho.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>INICIO - DL Store</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  
+  
+  </head>
+  <body>
+		
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark"><a href="index.php"><img src="img/logo.png" alt="logo image" style="height: 70px"></a>
+   <div class="container collapse navbar-collapse" id="navbarTogglerDemo02">
+<div class="container" >
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Produtos</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Serviços</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Endereço</a>
+      </li>
+    </ul>
+   </div></div>
+   
+   <form class="form-inline my-2 my-lg-0 w-75 p-1 " >
+   <div class=" row  " >
+   <div class="col" >
+      <input class="form-control" type="search"  placeholder="Pesquisar">
+	  </div>
+	     <div class="col-sm-4"   >
+      <button class="btn btn-outline-success" type="submit">Pesquisar</button>
+	  </div>
+	  </div>
+    </form>
+	
+</nav>
--- info.php
+++ info.php
@@ -1,45 +1,7 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>INICIO - DL Store</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  
-  
-  </head>
-  <body>
-		
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark"><a href="index.php"><img src="img/logo.png" alt="logo image" style="height: 50px"></a>
-<div class="container">
-   <div class="container collapse navbar-collapse" id="navbarTogglerDemo02">
-    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Produtos</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Serviços</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Endereço</a>
-      </li>
-    </ul>
-   </div></div>
-   
-   <form class="form-inline my-2 my-lg-0 w-75 p-3 " >
-   <div class="row ">
-   <div class="col "  >
-      <input class="form-control mr-sm-2" type="search"  placeholder="Pesquisar">
-	  </div>
-	     <div class="col" >
-      <button class="btn btn-outline-success" type="submit">Pesquisar</button>
-	  </div>
-	  </div>
-    </form>
-	
-</nav>
+<?php
 
+require_once("cabecalho.php");
+?>
 <div id="demo" class="carousel slide" data-bs-ride="carousel">
 	<div class="carousel-indicators">
 		<button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
@@ -84,19 +46,6 @@
 </br>	
 </br>	
 </br>
-<div class="container mt-3">	
-	<div class="dropdown">
-		<button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown">
-		Botao de escolha
-		</button>
-  <ul class="dropdown-menu">
-    <li><a class="dropdown-item" href="login.php">Link 1</a></li>
-    <li><a class="dropdown-item" href="#">Link 2</a></li>
-    <li><a class="dropdown-item" href="#">Link 3</a></li>
-  </ul>
-</div>
-</div>
-	
 	
 	
 	
