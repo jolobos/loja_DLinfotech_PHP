145795a33cca137c7c19f4170eb735c7ce3d9514
jolobos <digo8432@gmail.com>
2024-03-11 23:45:46 -0300

    testando outros cards responsivos

--- info.php
+++ info.php
@@ -41,9 +41,11 @@ require_once("cabecalho.php");
 	  </button>
 </div>
 </br>
+
+
 <div class="row" >
 <div class="col" >
-<div class="card mx-auto " style="width: 18rem;">
+<div class="card mx-auto w-auto p-3">
   <img class="card-img-top "  src="img/card1.png" alt="Imagem de capa do card">
   <div class="card-body">
     <h5 class="card-title">Carregadores</h5>
@@ -53,7 +55,7 @@ require_once("cabecalho.php");
 </div>
 </div>
 <div class="col">
-<div class="card  mx-auto" style="width: 18rem;">
+<div class="card  mx-auto w-auto p-3">
   <img class="card-img-top" src="img/card2.png" alt="Imagem de capa do card">
   <div class="card-body">
     <h5 class="card-title">Computadores</h5>
@@ -63,7 +65,7 @@ require_once("cabecalho.php");
 </div>
 </div>
 <div class="col">
-<div class="card  mx-auto" style="width: 18rem;">
+<div class="card  mx-auto w-auto p-3">
   <img class="card-img-top" src="img/card3.png" alt="Imagem de capa do card">
   <div class="card-body">
     <h5 class="card-title">Capinhas e Peliculas</h5>
@@ -72,7 +74,7 @@ require_once("cabecalho.php");
   </div>
 </div>
 </div><div class="col">
-<div class="card  mx-auto" style="width: 18rem;">
+<div class="card  mx-auto w-auto p-3" >
   <img class="card-img-top" src="img/card4.png" alt="Imagem de capa do card">
   <div class="card-body">
     <h5 class="card-title">Troca de display</h5>
@@ -81,7 +83,7 @@ require_once("cabecalho.php");
   </div>
 </div>
 </div><div class="col">
-<div class="card  mx-auto" style="width: 18rem;">
+<div class="card  mx-auto w-auto p-3">
   <img class="card-img-top" src="img/card5.png" alt="Imagem de capa do card">
   <div class="card-body">
     <h5 class="card-title">Acessórios</h5>
@@ -91,7 +93,116 @@ require_once("cabecalho.php");
 </div>
 </div>
 </div>
+</br>
+</br>
 
-	
-	</body>
+	<h1> testando cards diferentes</h1>
+	</br>
+<div id="carouselExampleCaptions" class="carousel carousel-dark slide" data-bs-ride="false">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+     <div class="card-group">
+      <div class="card">
+       <img src="..." class="card-img-top" alt="...">
+       <div class="card-body">
+       <h5 class="card-title">Card title</h5>
+       <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+       <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
+
+    </div>
+    <div class="carousel-item">
+<div class="card-group">
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
+
+    </div>
+    <div class="carousel-item">
+  
+
+<div class="card-group">
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+</body>
 </html>
