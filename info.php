f9062bc1d162d33d5f7e328b64aba432028a57ba
jolobos <digo8432@gmail.com>
2024-03-14 11:02:59 -0300

    cards com efeito responsivo

--- info.php
+++ info.php
@@ -41,168 +41,55 @@ require_once("cabecalho.php");
 	  </button>
 </div>
 </br>
+<div class="btn btn-button" style="background: black; width: 100%; height:80px">
 
 
-<div class="row" >
-<div class="col" >
-<div class="card mx-auto w-auto p-3">
-  <img class="card-img-top "  src="img/card1.png" alt="Imagem de capa do card">
-  <div class="card-body">
-    <h5 class="card-title">Carregadores</h5>
-    <p class="card-text">Encontre carregadores para todas as marcas e modelos em celulares, notebooks e computadores.</p>
-    <a href="#" class="btn btn-primary">Visitar</a>
-  </div>
-</div>
-</div>
-<div class="col">
-<div class="card  mx-auto w-auto p-3">
-  <img class="card-img-top" src="img/card2.png" alt="Imagem de capa do card">
-  <div class="card-body">
-    <h5 class="card-title">Computadores</h5>
-    <p class="card-text">Querendo efetuar um upgrade na carroça? click e veja as opções e valores.</p>
-    <a href="#" class="btn btn-primary">Visitar</a>
-  </div>
-</div>
-</div>
-<div class="col">
-<div class="card  mx-auto w-auto p-3">
-  <img class="card-img-top" src="img/card3.png" alt="Imagem de capa do card">
-  <div class="card-body">
-    <h5 class="card-title">Capinhas e Peliculas</h5>
-    <p class="card-text">Temos capinhas e peliculas para inumeros modelos e marcas, com o melhor preço.</p>
-    <a href="#" class="btn btn-primary">Visitar</a>
-  </div>
-</div>
-</div><div class="col">
-<div class="card  mx-auto w-auto p-3" >
-  <img class="card-img-top" src="img/card4.png" alt="Imagem de capa do card">
-  <div class="card-body">
-    <h5 class="card-title">Troca de display</h5>
-    <p class="card-text">Consulte nossos valores e marcas disponiveis. </p></br>
-    <a href="#" class="btn btn-primary">Visitar</a>
-  </div>
-</div>
-</div><div class="col">
-<div class="card  mx-auto w-auto p-3">
-  <img class="card-img-top" src="img/card5.png" alt="Imagem de capa do card">
-  <div class="card-body">
-    <h5 class="card-title">Acessórios</h5>
-    <p class="card-text">Temos inumeros acessórios para toda area de informatica e smartphones.</p>
-    <a href="#" class="btn btn-primary">Visitar</a>
-  </div>
-</div>
-</div>
-</div>
-</br>
-</br>
 
-	<h1> testando cards diferentes</h1>
-	</br>
-<div id="carouselExampleCaptions" class="carousel carousel-dark slide" data-bs-ride="false">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-     <div class="card-group">
-      <div class="card">
-       <img src="..." class="card-img-top" alt="...">
-       <div class="card-body">
-       <h5 class="card-title">Card title</h5>
-       <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-       <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-  <div class="card">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-  <div class="card">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
 </div>
+<section class="secao4" id="sobre">
+    <div class="secao4-div row">
+        <!-- Card 1 -->
+        <div class="secao4-div-card col">
+            <img decoding="async" src="img/card1.png" alt="imagem do card 1 html e css">
+			<h5 class="card-title">Carregadores</h5>
+			<p class="card-text">Encontre carregadores para todas as marcas e modelos em celulares, notebooks e computadores.</p>
+			<a href="#" class="btn btn-primary">Visitar</a>
+		</div>
 
-    </div>
-    <div class="carousel-item">
-<div class="card-group">
-  <div class="card">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-  <div class="card">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-  <div class="card">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-</div>
+        <!-- Card 2 -->
+        <div class="secao4-div-card col">
+            <img decoding="async" src="img/card2.png" alt="imagem do card 2 html e css">
+			<h5 class="card-title">Computadores</h5>
+			<p class="card-text">Querendo efetuar um upgrade na carroça? click e veja as opções e valores.</p>
+			</br>
+			<a href="#" class="btn btn-primary">Visitar</a>
+		</div>
 
-    </div>
-    <div class="carousel-item">
-  
+        <!-- Card 3 -->
+        <div class="secao4-div-card col">
+            <img decoding="async" src="img/card3.png" alt="imagem do card 3 html e css">
+			<h5 class="card-title">Capinhas e Peliculas</h5>
+			<p class="card-text">Temos capinhas e peliculas para inumeros modelos e marcas, com o melhor preço.</p>
+			</br>
+			<a href="#" class="btn btn-primary">Visitar</a>
 
-<div class="card-group">
-  <div class="card">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+		<div class="secao4-div-card col">
+            <img decoding="async" src="img/card4.png" alt="imagem do card 3 html e css">
+            <h5 class="card-title">Troca de display</h5>
+			<p class="card-text">Consulte nossos valores e marcas disponiveis. </p></br>
+			<a href="#" class="btn btn-primary">Visitar</a>
+        </div>
+		<div class="secao4-div-card col">
+            <img decoding="async" src="img/card5.png" alt="imagem do card 3 html e css">
+            <h5 class="card-title">Acessórios</h5>
+			<p class="card-text">Temos inumeros acessórios para toda area de informatica e smartphones.</p>
+			</br>
+			<a href="#" class="btn btn-primary">Visitar</a>
+        </div>
     </div>
-  </div>
-  <div class="card">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-  <div class="card">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-</div>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
+</section>
+
 
 </body>
 </html>
