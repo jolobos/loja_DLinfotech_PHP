f607f0fbc0f6be0315781c65bd32c65807219d7e
jolobos <digo8432@gmail.com>
2024-06-23 16:35:10 -0300

    iniciando area de compras da administracao

---
+++ administrador/compras/compras.php
@@ -0,0 +1,191 @@
+<?php
+require_once '../../database.php';
+require_once '../../verifica_session.php';
+error_reporting(E_ALL);
+ini_set('display_errors','on');
+date_default_timezone_set('America/Sao_Paulo');
+?>
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Produtos do sistema - DL Store</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="func_us.js"></script>
+	<link rel="stylesheet" href="../../css/modal.css">
+
+</head>
+  <body style="background: #778899">
+  <div class="container">
+				<div class="bg-dark"><h1 class="text-success">
+					Opções para Compras
+				</h1>
+				<button type="button" class="btn btn-info m-2" data-toggle="modal" data-target="#exampleModal">
+				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-menu-button-wide-fill" viewBox="0 0 16 16">
+				<path d="M1.5 0A1.5 1.5 0 0 0 0 1.5v2A1.5 1.5 0 0 0 1.5 5h13A1.5 1.5 0 0 0 16 3.5v-2A1.5 1.5 0 0 0 14.5 0zm1 2h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1 0-1m9.927.427A.25.25 0 0 1 12.604 2h.792a.25.25 0 0 1 .177.427l-.396.396a.25.25 0 0 1-.354 0zM0 8a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm1 3v2a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2zm14-1V8a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2zM2 8.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m0 4a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5"/>
+			</svg>MENU
+				</button>
+				<a class="btn btn-secondary border-info m-2" href="../menu_admin.php">Administração</a>
+				<a href="../../sair.php" class="btn btn-secondary border-info m-2">Sair</a>
+
+				</div>
+			    <div class="modal right fade" id="exampleModal" tabindex="" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-body">
+				<a class="btn btn-secondary border-danger m-2" href="usuarios.php">Usuários</a></br>
+				<a class="btn btn-secondary border-warning m-2" href="novos_usuarios.php">Novos usuários</a></br>
+				<a class="btn btn-secondary border-warning m-2" href="des_usuarios.php">Usuários desativados</a></br>
+				<a class="btn btn-secondary border-warning m-2" href="us_mais_cp.php">Usuários que mais compram</a></br>
+				<a class="btn btn-secondary border-warning m-2" href="us_menos_cp.php">Usuários que menos compram</a></br>
+				<a class="btn btn-secondary border-warning m-2" href="us_nunca_cp.php">Usuários que nunca compraram</a></br>
+				<a class="btn btn-secondary border-warning m-2" href="ex_auto.php">Exclusão automática</a></br>
+				
+				</div>
+				<div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                </div>
+				</div>
+				</div>
+				</div>
+	<div class="card mt-2">
+            <div class="card-header">
+                <h3 class="text-info">Pesquisa de compras do dia</h3>
+            </div>
+            <div class="card-body">
+                <?php
+                if(isset($_GET['ver_pr_compra'])){
+	$id_venda_lista = $_GET['ver_pr_compra'];
+	$sql_10 = "SELECT * FROM itens_da_compra WHERE id_compra = '".$id_venda_lista."'";
+	$consulta_10 = $conexao->query($sql_10);
+	$dados_ab = $consulta_10->fetchALL(PDO::FETCH_ASSOC);
+	
+	echo  '<div class="modal fade modal-lg" id="exemplomodal">
+  <div class="modal-dialog">
+    <div class="modal-content ">
+      <div class="modal-header bg-info">
+        <h3 class="modal-title">Lista de produtos da compra de n°: "'.$id_venda_lista.'"</h3>
+      </div>
+      <div class="modal-body bg-light">';
+		echo '<table class="table table-striped mt-2" border="3">
+                <thead>
+                    <tr align="center">
+                        <th colspan="8">Produtos da Compra</th>
+                    </tr>
+                    <tr>
+                        <th>Código do produto</th>
+                        <th>Foto do produto </th>
+                        <th>Produto</th>
+                        <th>Valor</th>
+                        <th>Quantidade</th>
+                        <th>Total</th>
+                        
+                    </tr>
+                </thead>
+                <tbody>';
+	$total_somado = 0;
+	foreach($dados_ab as $d_ab){
+	$id_produto_lista = $d_ab['id_produto'];
+	$sql_100 = "SELECT * FROM produtos WHERE id_produto = '".$id_produto_lista."'";
+	$consulta_100 = $conexao->query($sql_100);
+	$dados_abc = $consulta_100->fetch(PDO::FETCH_ASSOC);
+	
+	$cod_produto = $dados_abc['cod_produto'];
+        $nome = $dados_abc['nome'];
+        $valor= $dados_abc['valor'];
+        $quantidade= $d_ab['quantidade'];
+	$foto_pr= $dados_abc['foto'];
+	$total = $quantidade * $dados_abc['valor'];
+        $total_somado += $total;
+	echo '<tr style="height: 110px">
+                <td><P style="margin-top: 40px">'.$cod_produto.'</P></td>
+                <td><img style="width:100px "src="../../img/produtos/'.$foto_pr.'"> </td>
+                <td width="400"><P style="margin-top: 40px">'.$nome.'</P></td>
+                <td><P style="margin-top: 40px; width: 70px">R$: '. number_format($valor,2,',','.').'</P></td>
+                <td><P style="margin-top:40px ; width: 70px">'.$quantidade.' Un</P></td>
+                <td><P style="margin-top: 40px; width: 70px"> R$: '.number_format($total,2,',','.').'</P></td></tr>';
+	
+	}        
+		
+      echo '<tr><td align="right" colspan="6"><P >Total R$: '.number_format($total_somado,2,',','.').'</P></td></tr>
+
+          </tbody></table></div>
+      <div class="modal-footer bg-light">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+
+      </div>
+    </div>
+  </div>
+</div>';
+}
+                
+                
+                    $dia = date("Y-m-d");
+                    $sql = "SELECT * FROM compras WHERE data >= DATE('".$dia."')";
+                    $consulta = $conexao->query($sql);
+                    $dados = $consulta->fetchALL(PDO::FETCH_ASSOC);
+                    if(empty($dados)){
+                        echo '<h3 class="alert alert-secondary">Nenhuma compra foi efetuada na data de hoje</h3>';    
+                    }else{
+                        echo '<table  align="center" border="3" class="border-secondary" style="table-layout: fixed;width:1113px">';
+                        echo '<thead style="display: block;position: relative;" class="border">';
+                        echo '<tr>';
+
+                        echo '  <th width=200>Id. da compra</th>
+                        <th width=200>Pagamento</th>
+                        <th width=200>Endereço</th>
+                        <th width=200>Data da compra</th>
+                        <th width=200>Pago</th>
+                        <th width=200>Entregue</th>
+                        <th width=200>Total da compra</th>
+                        <th width=200>Ações</th>';
+                        echo '</tr>';
+                        echo '</thead>';
+                        echo '<tbody style="display: block;  overflow: auto;width: 100%;max-height: 400px;overflow-y: scroll;overflow-x: hidden;">';
+                        
+                    foreach ($dados as $d){
+                        $id_compra = $d['id_compra'];
+                                $id_endereco = $d['id_endereco'];
+				$sqlend = "SELECT * FROM endereco_usuario WHERE id_endereco = '".$id_endereco."'";
+				$consultaend = $conexao->query($sqlend);
+				$dados_end = $consultaend->fetch(PDO::FETCH_ASSOC);
+				$endereco = '<strong>R:</strong> '.$dados_end['logradouro'].' <strong>N°</strong> '.$dados_end['numero'].'<br><strong>B: </strong>'.$dados_end['bairro'].' <strong>Cidade:</strong> '.$dados_end['cidade'].'';
+                                
+                                $autorizado= $d['autorizado'];
+				if($autorizado == 0){ $pago = 'Não';
+                                    }else{
+                                            $pago = 'Sim';
+                                    }
+                                $entregue=$d['entregue'];
+                                    if($entregue == 0){ $ent = 'Não';
+                                    }else{
+                                            $ent = 'Sim';
+                                    }
+                       echo '<tr style="height: 110px">
+                            <td width=200><P class="mt-4">'.$d['id_compra'].'</P></td>
+                            <td width=200><P class="mt-4">'.$d['pagamento'].'</P></td>
+                            <td width=200><P class="mt-4">'.$endereco.'</P></td>
+                            <td width=200><P class="mt-4">'.date_format(new DateTime($d['data']),"d/m/Y").'</P></td>
+                            <td width=200><P class="mt-4">'.$pago.'</P></td>
+                            <td width=200><P class="mt-4">'.$ent.'</P></td>
+                            <td width=200><P class="mt-4"> R$: '.number_format($d['total'],2,',','.').'</P></td>
+                            <td width=200><a class="btn btn-success w-75 mt-2" href="?ver_pr_compra='.$id_compra.'">Ver Produtos</a>
+                            <a class="btn btn-dark mt-2 w-75" href="?cancelar_compra='.$id_compra.'">Cancelar Compra</a></td></tr>';
+                        
+                    }
+                    
+                    }
+                ?>
+            </div>
+            </div>
+<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<script type="text/javascript">
+$(window).load(function() {
+    $('#exemplomodal').modal('show');
+});
+</script>
--- administrador/menu_admin.php
+++ administrador/menu_admin.php
@@ -40,7 +40,7 @@ date_default_timezone_set('America/Sao_Paulo');
                     <path d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5l2.404.961L10.404 2zm3.564 1.426L5.596 5 8 5.961 14.154 3.5zm3.25 1.7-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464z"/>
                 </svg> - Configuração de produtos
             </a>
-			<a type="button" href="produtos/produtos.php" class="btn btn-info" style="width: 400px;margin: auto;margin-top: 10px">
+			<a type="button" href="compras/compras.php" class="btn btn-info" style="width: 400px;margin: auto;margin-top: 10px">
 		<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-bag-check" viewBox="0 0 16 16">
 			<path fill-rule="evenodd" d="M10.854 8.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L7.5 10.793l2.646-2.647a.5.5 0 0 1 .708 0"/>
 			<path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z"/>
