25ecc9ecded58f205e23da6b4c5646a9435f531e
jolobos <digo8432@gmail.com>
2024-10-18 08:37:11 -0300

    iniciando a recuperacao de senha via email, necessario criar uma conta de email para a loja.

---
+++ usuario/recupera_senha.php
@@ -0,0 +1,60 @@
+<?php
+session_start();
+error_reporting(E_ALL);
+ini_set('display_errors','on');
+date_default_timezone_set('America/Sao_Paulo');
+require_once('../database.php');
+$rdir = str_replace("\\", "/", __DIR__);                    //Root Dir
+require $rdir.'/email/src/Exception.php';
+require $rdir.'/email/src/PHPMailer.php';
+require $rdir.'/email/src/SMTP.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPOptions = array(
+        'ssl' => array(
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+        'allow_self_signed' => true
+        )
+        );
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'email do usuario';                     //SMTP username
+    $mail->Password   = 'Criar senha de aplicativo';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('digo8432@gmail.com', 'Digo');
+    $mail->addAddress('luana.koling@gmail.com', 'Digo');     //Add a recipient
+    //$mail->addAddress('ellen@example.com');               //Name is optional
+    //$mail->addReplyTo('info@example.com', 'Information');
+    //$mail->addCC('cc@example.com');
+    //$mail->addBCC('bcc@example.com');
+
+    //Para anexar arquivos
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Assunto';
+    $mail->Body    = 'Mensagem';
+    //$mail->AltBody = 'Testando oque é Altbody in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+
+?>
