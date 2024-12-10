7b5fed74dffb94da02a721abfa7a9ad243332936
jolobos <digo8432@gmail.com>
2024-12-10 16:50:13 -0300

    adicionado funcao de lista de entrega atraves do SGBD em seleciona_entrega.php

--- entregas/selecionar/seleciona_entrega.php
+++ entregas/selecionar/seleciona_entrega.php
@@ -4,21 +4,40 @@ require_once '../verifica_session.php';
 error_reporting(E_ALL);
 ini_set('display_errors','on');
 date_default_timezone_set('America/Sao_Paulo');
-if(!isset($_SESSION['entregas_selecionadas'])){
-    $_SESSION['entregas_selecionadas'] = array();
-}
 
 if(!empty($_POST['sel_entrega'])){
-    $id_ent = intval($_POST['sel_entrega']);
-    if(!isset($_SESSION['sel_entrega'][$id_ent])){
-        $_SESSION['sel_entrega'][$id_ent]=1;
-    }else{
-       $_SESSION['sel_entrega'][$id_ent] +=1;
-    }
-   
-}
-
-
+    $id_compra = $_POST['sel_entrega'];
+    $sql_YU = "SELECT * FROM compras WHERE id_compra = '".$id_compra."'";
+    $consulta_YU = $conexao->query($sql_YU);
+    $dados_YU = $consulta_YU->fetch(PDO::FETCH_ASSOC);
+    $id_usuario = $dados_YU['id_usuario'];
+    $id_endereco = $dados_YU['id_endereco'];
+    $saida = 0;
+    $chegada = 0;
+    $devolucao = 0;
+    $cancelamento = 0;
+    $observações = 'vazio';
+    $status = 0;
+    
+    $sql ='INSERT INTO entregas (id_entregador,id_compra,id_usuario,id_endereco,saida,chegada,devolucao,cancelamento,observacoes,status) values(?,?,?,?,?,?,?,?,?,?)';
+		try {
+		$insercao = $conexao->prepare($sql);
+		$ok = $insercao->execute(array ($id_entregador,$id_compra,$id_usuario,$id_endereco,$saida,$chegada,$devolucao,$cancelamento,$observações,$status));
+		}catch(PDOException $r){
+			//$msg= 'Problemas com o SGBD.'.$r->getMessage();
+			$ok = False;
+		}catch (Exception $r){//todos as exceções
+		$ok= False; 
+			
+		}
+		
+			if ($ok){
+				$msg= 'Seus dados foram alterados com sucesso!.';
+				}else{
+					$msg='Lamento, não foi possivel alterar seus dados!.'.$r->getMessage().'';
+			}
+		header('location:seleciona_entrega.php?mensagem='.$msg);
+} 
 ?>
 <!doctype html>
 <html lang="pt-br">
@@ -68,7 +87,11 @@ if(!empty($_POST['sel_entrega'])){
                                 <div style="max-height:400px" class="overflow-auto">
                                     ';
                     foreach ($dados_a as $a){
+                        $sql45 = "SELECT * FROM entregas WHERE id_compra = '".$a['id_compra']."'";
+			$consulta45 = $conexao->query($sql45);
+			$d45 = $consulta45->fetch(PDO::FETCH_ASSOC);
                         
+                        if(empty($d45['id_compra'])){
                         $sql2 = "SELECT * FROM endereco_usuario WHERE id_endereco = '".$a['id_endereco']."'";
 			$consulta2 = $conexao->query($sql2);
 			$d = $consulta2->fetch(PDO::FETCH_ASSOC);
@@ -116,13 +139,13 @@ if(!empty($_POST['sel_entrega'])){
                   <label class="form-check-label" for="endereco'.$d['id_endereco'].'">
                   <strong> Telefone de contato:</strong> '.$d['telefone_entrega'].' 
 		  </label></br>
-                  <input type="hidden" name="sel_entrega" value="'.$a['id_endereco'].'">
+                  <input type="hidden" name="sel_entrega" value="'.$a['id_compra'].'">
                   <div align="right">    
                   <input class="btn btn-primary me-4" type="submit" id="endereco'.$d['id_endereco'].'"  value="Selecionar">
                   </div>
                   </form><hr/>
                 ';                   
-                    }
+                    }}
                     echo '
                             </div>
                             </div>
@@ -134,16 +157,66 @@ if(!empty($_POST['sel_entrega'])){
                             <div class="card-header">
                                
                             </div>
-                            <div class="card-body" style="max-height:400px" class="overflow-auto">';
-                    if(isset($_SESSION['entregas_selecionadas'])){
-                        var_dump($_SESSION['entregas_selecionadas']);
-                        }
-                    
-                    echo '</div>
+                            <div class="card-body"><div style="max-height:400px" class="overflow-auto">';
+                            $sql93 = "SELECT * FROM entregas WHERE id_entregador = '".$id_entregador."'";
+                            $consulta93 = $conexao->query($sql93);
+                            $d93 = $consulta93->fetchALL(PDO::FETCH_ASSOC);
+
+                            foreach ($d93 as $s){
+                                $sql2 = "SELECT * FROM endereco_usuario WHERE id_endereco = '".$s['id_endereco']."'";
+                                $consulta2 = $conexao->query($sql2);
+                                $d = $consulta2->fetch(PDO::FETCH_ASSOC);
+
+                                $CEP = $d['CEP'];
+                                        $rua = $d['logradouro'];
+					$bairro = $d['bairro'];
+					$cidade = $d['cidade'];
+					$UF = $d['UF'];
+					$numero = $d['numero'];
+					$complemento = $d['complemento'];
+					$ponto_referencia = $d['ponto_referencia'];
+					$retirada_com = $d['retirada_com'];
+					$telefone_entrega = $d['telefone_entrega'];
+                                        
+							
+                                echo '
+                                  <label class="form-check-label" for="endereco'.$d['id_endereco'].'">
+                                  <strong>CEP:</strong> '.$d['CEP'].' 
+                                  </label>
+                                  <label class="form-check-label" for="endereco'.$d['id_endereco'].'">
+                                  <strong> Rua:</strong> '.$d['logradouro'].' 
+                                  </label>
+                                  <label class="form-check-label" for="endereco'.$d['id_endereco'].'">
+                                  <strong> Bairro:</strong> '.$d['bairro'].' 
+                                  </label>
+                                  <label class="form-check-label" for="endereco'.$d['id_endereco'].'">
+                                  <strong> Cidade:</strong> '.$d['cidade'].' 
+                                  </label>
+                                  <label class="form-check-label" for="endereco'.$d['id_endereco'].'">
+                                  <strong> UF:</strong> '.$d['UF'].' 
+                                  </label></br>
+                                  <label class="form-check-label" for="endereco'.$d['id_endereco'].'">
+                                  <strong> n°:</strong> '.$d['numero'].' 
+                                  </label>
+                                  <label class="form-check-label" for="endereco'.$d['id_endereco'].'">
+                                  <strong> Complemento:</strong> '.$d['complemento'].' 
+                                  </label>
+                                  <label class="form-check-label" for="endereco'.$d['id_endereco'].'">
+                                  <strong> Ponto de referência:</strong> '.$d['ponto_referencia'].' 
+                                  </label></br>
+                                  <label class="form-check-label" for="endereco'.$d['id_endereco'].'">
+                                  <strong> Responsável pela retirada:</strong> '.$d['retirada_com'].' 
+                                  </label>
+                                  <label class="form-check-label" for="endereco'.$d['id_endereco'].'">
+                                  <strong> Telefone de contato:</strong> '.$d['telefone_entrega'].' 
+                                  </label></br>
+                                  <hr/>
+                                ';                   
+                                    }    
+
+                    echo '</div></div>
                             <div class="card-footer">
-                            </div>
-                ';
-                    
+                            </div>';
                     
                     echo '</div>';
                     
