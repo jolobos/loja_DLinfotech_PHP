4ed9dbbdf904a7de608d865e45c9e7a98e30c691
jolobos <digo8432@gmail.com>
2024-12-19 16:43:53 -0300

    continuando entregas, resolvido bug do cancelar entrega e iniciado o finalizar entrega.

--- entregas/entregas/finalizar_entrega.php
+++ entregas/entregas/finalizar_entrega.php
@@ -5,17 +5,55 @@ error_reporting(E_ALL);
 ini_set('display_errors','on');
 date_default_timezone_set('America/Sao_Paulo');
 if($_POST['concluir_ent']){
+    $id_compra = $_POST['concluir_ent'];
     $nome_entrega = $_POST['nome_entrega'];
     $CPF_entrega = $_POST['CPF_entrega'];
+    if($_POST['parente_entrega']){
     $parente_entrega = $_POST['parente_entrega'];
+    }else{
+        $parente_entrega = '';
+    }
+    if($_POST['obs_entrega']){
     $obs_entrega = $_POST['obs_entrega'];
+    }else{
+        $obs_entrega = '';
+    }
     $sql455z = "SELECT * FROM entregas WHERE id_compra = '".$id_compra."'";
     $consulta455z = $conexao->query($sql455z);
     $b = $consulta455z->fetch(PDO::FETCH_ASSOC);
-
     
+    $remover = array($id_compra);
+    $_SESSION['ordem_etinerario'] = array_diff($_SESSION['ordem_etinerario'], $remover);
+    $zero = 1;
+    $zera_hor =  date('Y-m-d H:i:s');
+    $sql ='UPDATE entregas SET ordem_ent=?,chegada=?,hora_chegada=?,status=? WHERE id_compra = '.$id_compra.'';
+                try {
+               $insercao = $conexao->prepare($sql);
+                $ok2 = $insercao->execute(array ($zero,$zero,$zera_hor,$zero));
+                }catch(PDOException $r){
+                //$msg= 'Problemas com o SGBD.'.$r->getMessage();
+                        $ok2 = False;
+                    }catch (Exception $r){//todos as exceções
+                        $ok2= False; 
+                    }
+    if($ok2){   
+        $conttt = 1;
+    foreach ($_SESSION['ordem_etinerario'] as $vish){
+               
+               $sql ='UPDATE entregas SET ordem_ent=? WHERE id_compra = '.$vish.'';
+                try {
+               $insercao = $conexao->prepare($sql);
+                $ok1 = $insercao->execute(array ($conttt));
+                }catch(PDOException $r){
+                //$msg= 'Problemas com o SGBD.'.$r->getMessage();
+                        $ok1 = False;
+                    }catch (Exception $r){//todos as exceções
+                        $ok1= False; 
+                    }
+                $conttt++;}
     
-    
+       
+}
 }
 
 ?>
