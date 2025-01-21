2422a355e5fca29333fa203a54b7b55691d3a956
jolobos <digo8432@gmail.com>
2025-01-21 14:03:36 -0300

    ver_rend_periodo nao esta mostrando o grafico, problema no javascript.

--- entregas/rendimentos/ver_rend_periodo.php
+++ entregas/rendimentos/ver_rend_periodo.php
@@ -41,7 +41,114 @@ date_default_timezone_set('America/Sao_Paulo');
         <div class="card-header">
             <h3 class="text-primary">Performace do entregador</h3>
         </div>
-        <div class="card-body"></div>
+        <div class="card-body">
+            <form method="POST">
+                <div class="row">
+                <div class="col-sm-2">
+                <input class="form-control" type="date" name="periodo_1">
+                </div>
+                <div class="col-sm-1 m-auto" align="center">
+                    <label>Até</label>
+                </div>
+                <div class="col-sm-2">
+                <input class="form-control" type="date" name="periodo_2">
+                </div>
+                <div class="col">
+                    <input type="submit" class="btn btn-success" value="Pesquisar">
+                </div>
+                </div>
+                </form>
+            <hr>
+            <?php
+                if(!empty($_POST['periodo_1']) && !empty($_POST['periodo_2'])){
+                    if($_POST['periodo_1'] <= $_POST['periodo_2']){
+                    $data_1 = $_POST['periodo_1'];
+                    $data_2 = $_POST['periodo_2'];
+                    $sqlz = "SELECT COUNT(id_entregas) as contagem FROM entregas WHERE id_entregador = '".$id_entregador."' AND hora_chegada >= '".$data_1."' AND hora_chegada <= '".$data_2."' AND status = 1";
+                    $consultaz = $conexao->query($sqlz);
+                    $az = $consultaz->fetch(PDO::FETCH_ASSOC);
+                    echo '<h3 class="alert alert-success">Quantidade total de entregas nesse periodo "'.$az['contagem'].'"</h3><br>';
+                    
+                    $sqly = "SELECT DAY(hora_chegada) AS dia, COUNT(id_entregador) AS cont FROM entregas WHERE id_entregador = '".$id_entregador."' AND hora_chegada >= '".$data_1."' AND hora_chegada <= '".$data_2."' AND status = 1 GROUP BY dia";
+                    $consultay = $conexao->query($sqly);
+                    $ay = $consultay->fetchALL(PDO::FETCH_ASSOC);
+                    foreach($ay as $ai){
+                        $data[] = array('mes_arr' => $ai['cont'],'valor' => $ai['dia']);
+                    }
+                    $new_data = array_column($data,'mes_arr','valor');
+                    print_r($new_data);
+                    echo '<canvas id="myChart" width="1600" height="300"></canvas>';
+                    }else{
+                    echo '<h3 class="alert alert-danger">Data selecionada está incorreta para pesquisa!</h3>';
+    
+                    }
+                }else{
+                    echo '<h3 class="alert alert-danger">Data selecionada está incorreta para pesquisa!</h3>';
+                }
+            ?>
+        </div>
     </div>
   </div>
+<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+<script>
+    
+      // unica diferença é que você receberá o json dinamicamente
+      // valor que chegará da requisição            
+      //let json = <?php echo json_encode($new_data); ?>
+      let json = JSON.parse('{ "jose":3 , "maria":2, "joão":3 , "pedro":4}');
+      var ano2 = <?php echo $ano; ?>
+      // cria uma array para nomes e valore
+      let nomes = [];
+      let valores = [];
+
+      // percorre o json
+      for(let i in json){
+         // adiciona na array nomes a key do campo do json
+         nomes.push(i);
+         // adiciona na array de valore o value do campo do json
+         valores.push(json[i]);
+      }
+
+      var ctx = document.getElementById('myChart').getContext('2d');
+      var myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+          //labels são cada uma das barrinhas. Basta adicionar a array abaixo:
+          labels: nomes,
+          datasets: [{
+              label: 'Quantidade de entregas para o periodo',
+              //data serve para adicionar o valor de cada barrinha. Basta adicionar a array abaixo:
+              data: valores,
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero: true
+                  }
+              }]
+          }
+      }
+  });
+   
+
+</script>
 </body>
